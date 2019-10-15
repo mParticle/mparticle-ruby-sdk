95f8f08077686b6a6275496386096ef1819d1e83
Alexander Sapountzis <alexs@mparticle.com>
2019-10-15 11:18:43 -0400

    Add Rspec tests

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,34 @@
+# require 'mparticle/models/product'
+require 'mparticle'
+
+describe 'Product' do
+  it 'should create an instance of Product' do
+    model = MParticle::Product.new
+    # puts model
+    expect(model).to be_an_instance_of MParticle::Product
+  end
+
+  it 'should have the property id (base name: "id")' do
+    model = MParticle::Product.new
+    model.id = '12345'
+    expect(model.id).to eq '12345'
+  end
+
+  it 'should have the property name (base name: "name")' do
+    model = MParticle::Product.new
+    model.name = 'Laserdisc'
+    expect(model.name).to eq 'Laserdisc'
+  end
+
+  it 'should have the property price (base name: "price")' do
+    model = MParticle::Product.new
+    model.price = 100.59 
+    expect(model.price).to eq 100.59 
+  end
+
+  it 'should have the property totalProductAmount (base name: "total_product_amount")' do
+    model = MParticle::Product.new
+    model.total_product_amount = 40.10 
+    expect(model.total_product_amount).to eq 40.10 
+  end
+end
